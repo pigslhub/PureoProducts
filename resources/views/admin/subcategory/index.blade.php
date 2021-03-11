e0f16d2ee97ebf697e3a7244b387cdc4e32ebafa
pigslhub <59114946+pigslhub@users.noreply.github.com>
2021-03-11 14:49:40 +0500

    changes

--- resources/views/admin/subcategory/index.blade.php
+++ resources/views/admin/subcategory/index.blade.php
@@ -23,28 +23,29 @@
                     <div class="card-body">
                         <div class="table-responsive">
                             <div class="row">
-                            @forelse (_getAllShopCategories() as $category)
+                            @forelse(_getAllShopCategories() as $category)
                                     <div class="col-md-4">
                                         <div class="card user-card">
                                         <div class="card-body">
                                             <div class="online-user">
 
                                             </div>
-                                            <div class="user-card-image"><img class="img rounded-circle image-radius" src="{{asset('storage/'.$category->icon)}}" width="100" height="100" ></div>
+                                            <div class="user-card-image">
+                                                @if($category->icon == null || $category->icon=='')
+                                                <img src="{{asset('assets/images/noImg.jpg')}}" style="height:80px;width:80px;border-radius:50%">
+                                                @else
+                                                <img src="{{asset('storage/'.$category->icon)}}" style="height:80px;width:80px;border-radius:50%">
+                                                @endif
+                                            </div>
                                             <div class="user-deatils text-center">
                                             <h5>{{$category->name}}</h5>
-{{--                                             <h6 class="mb-0">marshikisteen@gmail.com</h6>--}}
                                             <a href="{{route('subcategory.createField',$category->id)}}" class="btn btn-sm btn-primary" type="button">View</a>
-{{--                                             <a href="{{route('adminSubCategories.store',['category' => $category->id])}}"--}}
-{{--                                                class="btn btn-sm btn-primary" title="Apply for Shop-Type"><i--}}
-{{--                                                    class="fa fa-plus"></i></a>--}}
                                             </div>
 
                                         </div>
                                         </div>
                                     </div>
                                 @empty
-
                                 @endforelse
                             </div>
                         </div>
