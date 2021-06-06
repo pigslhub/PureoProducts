00e376009b5dad07bce324f015047b951d710803
pigslhub <pigslhub@gmail.com>
2021-06-06 15:26:41 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,26 @@
 |
 */
 
- Route::get('/', function () {
-     return view('frontend.index');
- });
+//  Route::get('/', function () {
+//      return view('frontend.index');
+//  });
 
-Route::group(['namespace' => 'Frontend'], function(){
-    Route::get('/dashboard', ['uses' => 'FrontendController@dashboard' , 'as' => 'frontend.dashboard' ]);
-    Route::get('/productsPage/{subCategory}', ['uses' => 'FrontendController@productsPage' , 'as' => 'frontend.products']);
-    Route::get('/subcategories/{category}', ['uses' => 'FrontendController@subcategories' , 'as' => 'frontend.subcategories']);
-    Route::get('/productDetails/{product}', ['uses' => 'FrontendController@productDetails' , 'as' => 'frontend.productDetails']);
-    Route::get('/yourCart/{id}', ['uses' => 'FrontendController@yourCart' , 'as' => 'frontend.yourCart']);
-    Route::get('/checkout', ['uses' => 'FrontendController@checkout' , 'as' => 'frontend.checkout']);
-    Route::get('/allOngoingOrders', ['uses' => 'OrderController@allOngoingOrders' , 'as' => 'frontend.allOngoingOrders']);
-    Route::get('/allCompletedOrders', ['uses' => 'OrderController@allCompletedOrders' , 'as' => 'frontend.allCompletedOrders']);
-    Route::get('/allOngoingOrderCarts/{order}', ['uses' => 'OrderController@allOngoingOrderCarts' , 'as' => 'frontend.allOngoingOrderCarts']);
-    Route::get('/allCompletedOrderCarts/{order}', ['uses' => 'OrderController@allCompletedOrderCarts' , 'as' => 'frontend.allCompletedOrderCarts']);
-    Route::resource('carts','FrontendCartController');
-    Route::post('checkoutsession', 'PaymentController@createSession')->name('frontend.checkoutpayment');
-});
+Route::get('/', ['uses' => 'Admin\Auth\LoginController@showLoginForm' , 'as' => 'admin.login']);
+
+// Route::group(['namespace' => 'Frontend'], function(){
+//     Route::get('/dashboard', ['uses' => 'FrontendController@dashboard' , 'as' => 'frontend.dashboard' ]);
+//     Route::get('/productsPage/{subCategory}', ['uses' => 'FrontendController@productsPage' , 'as' => 'frontend.products']);
+//     Route::get('/subcategories/{category}', ['uses' => 'FrontendController@subcategories' , 'as' => 'frontend.subcategories']);
+//     Route::get('/productDetails/{product}', ['uses' => 'FrontendController@productDetails' , 'as' => 'frontend.productDetails']);
+//     Route::get('/yourCart/{id}', ['uses' => 'FrontendController@yourCart' , 'as' => 'frontend.yourCart']);
+//     Route::get('/checkout', ['uses' => 'FrontendController@checkout' , 'as' => 'frontend.checkout']);
+//     Route::get('/allOngoingOrders', ['uses' => 'OrderController@allOngoingOrders' , 'as' => 'frontend.allOngoingOrders']);
+//     Route::get('/allCompletedOrders', ['uses' => 'OrderController@allCompletedOrders' , 'as' => 'frontend.allCompletedOrders']);
+//     Route::get('/allOngoingOrderCarts/{order}', ['uses' => 'OrderController@allOngoingOrderCarts' , 'as' => 'frontend.allOngoingOrderCarts']);
+//     Route::get('/allCompletedOrderCarts/{order}', ['uses' => 'OrderController@allCompletedOrderCarts' , 'as' => 'frontend.allCompletedOrderCarts']);
+//     Route::resource('carts','FrontendCartController');
+//     Route::post('checkoutsession', 'PaymentController@createSession')->name('frontend.checkoutpayment');
+// });
 
 Auth::routes(['verify' => true]);
 //Route::view('/', 'default')->name('/');
