f44796c51b445beb5552e401923fba1844e08351
pigslhub <pigslhub@gmail.com>
2021-06-12 08:13:21 -0400

    update

--- storage/framework/views/8c45c747677fc4f476c8d1806b7f57ba8c7d0aac.php
+++ storage/framework/views/8c45c747677fc4f476c8d1806b7f57ba8c7d0aac.php
@@ -1,7 +1,7 @@
 <!-- Page Sidebar Start-->
 <div class="page-sidebar">
     <div class="main-header-left d-none d-lg-block">
-        <h4 style="color: white" class="text-center">PureO Natural Products</h4>
+        <h4 style="color: white; margin-top: 15px" class="text-center">Insaf Gift Center</h4>
 
 
     </div>
@@ -9,19 +9,20 @@
         <div class="sidebar-user text-center">
             <div><img class="img-60 rounded-circle" src="<?php echo e(auth('admin')->user()->getAvatarPath()); ?>" alt="#"></div>
             <h6 class="mt-3 f-14"><?php echo e(auth('admin')->user()->name); ?></h6>
-            <p><?php echo e(auth('admin')->user()->zip_code); ?></p>
         </div>
         <ul class="sidebar-menu">
             <li><a class="sidebar-header" href="<?php echo e(route('admin.dashboardPos')); ?>"><i data-feather="grid"></i><span>Dashboard</span></a></li>
-            <li><a class="sidebar-header" href="<?php echo e(route('adminCategories.index')); ?>"><i
-                        data-feather="bar-chart-2"></i><span>Categories</span></a></li>
-            <li><a class="sidebar-header" href="<?php echo e(route('adminSubCategories.index')); ?>"><i data-feather="server"></i><span>Sub Categories</span></a>
-            </li>
-            <li><a class="sidebar-header" href="<?php echo e(route('adminProducts.index')); ?>"><i data-feather="server"></i><span>Products</span></a>
+
+
+
+
+            <li><a class="sidebar-header" href="<?php echo e(route('products.index')); ?>"><i data-feather="server"></i><span>Products</span></a>
             </li>
             <!-- <li><a class="sidebar-header" href="<?php echo e(route('adminShops.index')); ?>"><i data-feather="home"></i><span>Shop Management</span></a>
             </li> -->
-            <li><a class="sidebar-header" href="<?php echo e(route('orders.loadAllOrders')); ?>"><i data-feather="package"></i><span>Order</span></a>
+            <li><a class="sidebar-header" href="<?php echo e(route('orders.index')); ?>"><i data-feather="package"></i><span>Order</span></a>
+            </li>
+            <li><a class="sidebar-header" href="<?php echo e(route('earnings.viewTotalEarning')); ?>"><i data-feather="dollar-sign"></i><span>Revenue</span></a>
             </li>
 
 
